cf48240a1bb0ca9d9e00075775f927dab352e2bf
thongster <106802301+thongster@users.noreply.github.com>
2026-01-04 19:08:42 +0700

    basic html templates

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const http = require('http')
+const fs = require('fs');
+
 
 const server = http.createServer((req, res) => {
     res.end('Hello')
 })
 
 server.listen(8080, () => {
-    console.log("Server is running")
+    console.log("Server is running on http://localhost:8080/")
 })
