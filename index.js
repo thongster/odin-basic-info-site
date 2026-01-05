01ba909bc8a9388f8a4a4dedbe5020d9922721df
thongster <106802301+thongster@users.noreply.github.com>
2026-01-05 11:51:47 +0700

    changed url path to just the name, which then read the correct readFile html file

--- index.js
+++ index.js
@@ -3,28 +3,33 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const urlPath = req.url;
+    console.log(urlPath)
 
-    if (urlPath === './index.html' || urlPath === '/') {
+    if (urlPath === '/') {
         fs.readFile("./index.html", (err, data) => {
         if (err) {
             res.statusCode = 404;
             res.end('server error')
+            return
         }
         res.end(data)
+
         });
-    } else if (urlPath === './about.html') {
+    } else if (urlPath === '/about') {
         fs.readFile("./about.html", (err, data) => {
         if (err) {
             res.statusCode = 404;
             res.end('server error')
+            return
         }
         res.end(data)
         });
-    } else if (urlPath === './contact-me.html') {
+    } else if (urlPath === '/contact-me') {
         fs.readFile("./contact-me.html", (err, data) => {
         if (err) {
             res.statusCode = 404;
             res.end('server error')
+            return
         }
         res.end(data)
         });
@@ -33,12 +38,15 @@ const server = http.createServer((req, res) => {
         if (err) {
             res.statusCode = 404;
             res.end('server error')
+            return
         }
         res.end(data)
         });
     }
-
+    
 })
+
+
 
 const PORT = process.env.PORT || 8080;
 
