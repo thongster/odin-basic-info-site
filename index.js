7c29edb7aca9c59786fe0670024f5f7c18d6e129
thongster <106802301+thongster@users.noreply.github.com>
2026-01-06 10:37:11 +0700

    added express and tried routing with static

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
+app.use(express.static("./"));
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+// app.get("/", (req, res) => res.send("index.html"));
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
@@ -15,61 +16,59 @@ app.listen(PORT, (error) => {
 });
 
 
-const http = require('http')
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    const urlPath = req.url;
-    console.log(urlPath)
+// const server = http.createServer((req, res) => {
+//     const urlPath = req.url;
+//     console.log(urlPath)
 
-    if (urlPath === '/') {
-        fs.readFile("./index.html", (err, data) => {
-        if (err) {
-            res.statusCode = 404;
-            res.end('server error')
-            return
-        }
-        res.end(data)
+//     if (urlPath === '/') {
+//         fs.readFile("./index.html", (err, data) => {
+//         if (err) {
+//             res.statusCode = 404;
+//             res.end('server error')
+//             return
+//         }
+//         res.end(data)
 
-        });
-    } else if (urlPath === '/about') {
-        fs.readFile("./about.html", (err, data) => {
-        if (err) {
-            res.statusCode = 404;
-            res.end('server error')
-            return
-        }
-        res.end(data)
-        });
-    } else if (urlPath === '/contact-me') {
-        fs.readFile("./contact-me.html", (err, data) => {
-        if (err) {
-            res.statusCode = 404;
-            res.end('server error')
-            return
-        }
-        res.end(data)
-        });
-    } else if (urlPath === "/style.css") {
-        fs.readFile("./style.css", (err, data) => {
-            if (err) {
-                res.statusCode = 404;
-                res.end("CSS not found");
-                return;
-            }
+//         });
+//     } else if (urlPath === '/about') {
+//         fs.readFile("./about.html", (err, data) => {
+//         if (err) {
+//             res.statusCode = 404;
+//             res.end('server error')
+//             return
+//         }
+//         res.end(data)
+//         });
+//     } else if (urlPath === '/contact-me') {
+//         fs.readFile("./contact-me.html", (err, data) => {
+//         if (err) {
+//             res.statusCode = 404;
+//             res.end('server error')
+//             return
+//         }
+//         res.end(data)
+//         });
+//     } else if (urlPath === "/style.css") {
+//         fs.readFile("./style.css", (err, data) => {
+//             if (err) {
+//                 res.statusCode = 404;
+//                 res.end("CSS not found");
+//                 return;
+//             }
 
-            res.writeHead(200, { "Content-Type": "text/css" });
-            res.end(data);
-        });
-    } else {
-        fs.readFile("./404.html", (err, data) => {
-        if (err) {
-            res.statusCode = 404;
-            res.end('server error')
-            return
-        }
-        res.end(data)
-        });
-    }
+//             res.writeHead(200, { "Content-Type": "text/css" });
+//             res.end(data);
+//         });
+//     } else {
+//         fs.readFile("./404.html", (err, data) => {
+//         if (err) {
+//             res.statusCode = 404;
+//             res.end('server error')
+//             return
+//         }
+//         res.end(data)
+//         });
+//     }
     
-})
+// })
